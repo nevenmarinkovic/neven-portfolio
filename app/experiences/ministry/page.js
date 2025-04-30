ed920558ea1ffa7ccbfd19548224ea126b1a1d04
nevenmarinkovic <neven.marinkovic@umconnect.umt.edu>
2025-04-30 10:17:47 -0600

    Updates

--- app/experiences/ministry/page.js
+++ app/experiences/ministry/page.js
@@ -4,6 +4,7 @@ import Link from "next/link";
 import Navbar from "@/components/navbar";
 import { useRef } from "react";
 import HeroSection from "@/components/heroSection";
+import ExperienceBody from "@/components/experienceBody";
 
 
 
@@ -18,7 +19,32 @@ export default function Page() {
             secondText={"It's more than Sunday"}
             >
             </HeroSection>
+
+          <ExperienceBody
+            imageSrc1={"/Campus.jpg"}
+            alternative1={"Campus"}
+            imageSrc2={"/WorshipNight.jpg"}
+            alternative2={"Worship Night"}
+            heading1={"My time at Resonate"}
+            heading2={"What I learned"}
+            text1=
+            {"Paragraph 1"
+
+            }
+            text2={"Paragraph 2"
+              }
+            text3={"Paragraph 3"
+            }
+            
+              text4={"Paragraph 4"
+              }
+              >
+            </ExperienceBody>
+
+
         </div>
+
+
 
             )
   }
